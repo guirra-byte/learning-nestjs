30ee7ecc54efea8efd602abe9a4665c97633ab20
Matheus Guirra Sousa ✝️ <guirramatheus1@gmail.com>
2022-11-03 22:34:01 +0000

    feat: finalizando basic CRUD

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -17,17 +17,23 @@ export class PlayersController {
   @HttpCode(200)
   @Post()
   async createUpdatePlayer(@Body() createPlayerDto: CreatePlayerDto) {
-    return this.playersService.createUpdatePlayer(createPlayerDto);
+    return await this.playersService.createUpdatePlayer(createPlayerDto);
   }
 
   @Get(':id')
   async findPlayer(@Param() playerId: string) {
-    return JSON.stringify({ playerId });
+    return await this.playersService.findPlayer(playerId);
+  }
+
+  @Get(':email')
+  async findPlayerByEmail(@Param() playerEmail: string) {
+    return await this.playersService.findPlayerByEmail(playerEmail);
   }
 
   @Get()
   async findAllPlayers() {
-    return JSON.stringify([{ name: 'Matheus Guirra Sousa' }]);
+    const players = await this.findAllPlayers();
+    return players;
   }
 
   @Patch(':id')
@@ -35,45 +41,11 @@ export class PlayersController {
     @Param() playerId: string,
     @Body() props: CreatePlayerDto,
   ) {
-    const player = {
-      name: 'Matheus',
-      phoneNumber: '92839756',
-      email: 'guirramatheus1@gmail.com',
-      id: playerId,
-    };
-
-    Object.assign(player, props);
-    return player;
+    await this.updatePlayer(playerId, props);
   }
 
   @Delete(':id')
   async removePlayer(@Param() playerId: string) {
-    const players = [
-      {
-        name: 'Matheus',
-        phoneNumber: '92839756',
-        email: 'guirramatheus1@gmail.com',
-        id: '1',
-      },
-      {
-        name: 'Matheus',
-        phoneNumber: '92839756',
-        email: 'guirramatheus1@gmail.com',
-        id: '2',
-      },
-    ];
-
-    for (let i = 0; i <= players.length; i++) {
-      const player = players[i];
-      const { id } = player;
-
-      if (id === playerId) {
-        return JSON.stringify(player);
-      }
-
-      return undefined;
-    }
-
-    return players;
+    await this.removePlayer(playerId);
   }
 }
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -2,18 +2,28 @@ import { Injectable, Logger } from '@nestjs/common';
 import { CreatePlayerDto } from './dtos/create-player.dto';
 import { Player } from './interfaces/player.interface';
 import { v4 as uuidV4 } from 'uuid';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class PlayersService {
   private logger: Logger = new Logger(PlayersService.name);
   private players: Player[] = [];
 
-  createUpdatePlayer(createUserDto: CreatePlayerDto) {
+  async createUpdatePlayer(createUserDto: CreatePlayerDto) {
     this.create(createUserDto);
   }
 
-  private create(createPlayerDto: CreatePlayerDto): Player {
+  private async create(createPlayerDto: CreatePlayerDto): Promise<Player> {
     const { name, email, password, phoneNumber } = createPlayerDto;
+
+    const ensureEmailExists = this.players.find(
+      (player) => player.email === email,
+    );
+
+    if (ensureEmailExists) {
+      await this.updatePlayer(ensureEmailExists.id, createPlayerDto);
+    }
+
     const player: Player = {
       id: uuidV4(),
       name,
@@ -28,5 +38,43 @@ export class PlayersService {
     this.logger.log(`createUpdatePlayer: ${JSON.stringify(player)}`);
     this.players.push(player);
     return player;
+  }
+
+  async findPlayer(id: string): Promise<Player[] | Player> {
+    const user = this.players.find((player) => player.id === id);
+
+    if (!user) {
+      return this.players;
+    }
+
+    return user;
+  }
+
+  async findPlayerByEmail(email: string): Promise<Player> {
+    return this.players.find((player) => player.email === email);
+  }
+
+  async findAllPlayers(): Promise<Player[]> {
+    return this.players;
+  }
+
+  async updatePlayer(playerId: string, props: CreatePlayerDto): Promise<void> {
+    const getUser = this.players.find((user) => user.id === playerId);
+
+    if (!getUser) {
+      throw new NotFoundException(`Cannot find player by #${playerId} id!`);
+    }
+
+    Object.assign(getUser, props);
+  }
+
+  async removePlayer(id: string): Promise<void> {
+    const getUserIndex = this.players.findIndex((player) => player.id === id);
+
+    if (getUserIndex === -1) {
+      throw new NotFoundException(`Cannot dins player by #${id} id!`);
+    }
+
+    throw new NotFoundException(`Cannot find player by #${id} id!`);
   }
 }
